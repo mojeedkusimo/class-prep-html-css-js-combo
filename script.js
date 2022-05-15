0052a99381f84d9575c53490c4ffdec906821a1e
mojeedkusimo <mkusimo90@gmail.com>
2022-05-15 03:38:19 +0100

    Completed initial functionality of selecting image

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let images = [
     'benin-ringroad.jpg',
-    'kano.jpg',
     'lagos-bridge.jpg',
     'lagos-skyline.jpg',
     'national-theatre.jpg'
 ];
+
+let button = document.querySelector("button");
+let imgTag = document.querySelector("img");
+let counter = 1;
+
+button.addEventListener("click", function() {
+    if ( counter === 4 ) {
+        counter = 0;
+    }
+    imgTag.src="./img/" + images[counter];
+    counter = counter + 1;
+
+});
